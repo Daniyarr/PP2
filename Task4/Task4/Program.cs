31cccdda2456c97d7d78f7e472a9ab063902674e
Daniyarr <43133652+Daniyarr@users.noreply.github.com>
2019-01-30 13:49:49 +0600

    Task4 have done it

---
+++ Task4/Task4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());                 // we change string to char than char to integer
+            for(int i = 1;i <= n; i++)                             //than we 
+            {
+                int t = i;
+                while(t != 0)
+                {
+                    Console.Write("[*]");
+                    t--;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
