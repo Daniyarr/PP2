490195bf2c33e9d5ea8de7258a11ef0f0c605c51
Daniyarr <43133652+Daniyarr@users.noreply.github.com>
2019-02-14 01:20:42 +0600

    Create Program.cs

---
+++ Week1/Task3/Task3/Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());              //создаем переменную n
+            string s = Console.ReadLine();
+            string[] a = s.Split();                             //эелементы строки делаю через пробелы
+            for(int i = 0;i < n; i++)                           
+            {
+                Console.Write(a[i] + " " + a[i] + " ");         //вывожу дважды элементы
+            }
+        }
+    }
+}
