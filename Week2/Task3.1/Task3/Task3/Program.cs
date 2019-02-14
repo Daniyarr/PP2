e5501609a510132a083f75d8220599a0f7eede3e
Daniyarr <43133652+Daniyarr@users.noreply.github.com>
2019-02-14 18:05:47 +0600

    Create Program.cs

---
+++ Week2/Task3.1/Task3/Task3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task3
+{
+    class Program
+    {
+        public static void Probels(int x) //создал функцию что бы создавать пробелы
+        {
+            for (int i = 0; i < x; i++) //проходя по циклу я создаю определенное количество пробелов
+            {
+                Console.Write("     ");
+            }
+        }
+        public static void Folders(DirectoryInfo dir, int x)
+        {
+            foreach (FileInfo files in dir.GetFiles()) //пробегаемся по всем файлам в указанной Directory 
+            {
+                Probels(x); //вызываем функцию пробелов
+                Console.WriteLine(files.Name); //и выводим имя файла
+            }
+            foreach (DirectoryInfo d in dir.GetDirectories()) //пробегаемся по всем папкам в указанной Directory
+            {
+                Probels(x); //вызываем функцию пробелов
+                Console.WriteLine(d.Name); //выводим имя папки
+                Folders(d, x + 1); //и обратно рекурсивно обращаемся к этой папке
+            }
+        }
+        static void Main(string[] args)
+        {
+            DirectoryInfo d = new DirectoryInfo(@"C:\Users\Professional\Desktop\c#"); //путь файла 
+
+            Folders(d, 0); //вызываем функцию рекурсию
+        }
+    }
+}
