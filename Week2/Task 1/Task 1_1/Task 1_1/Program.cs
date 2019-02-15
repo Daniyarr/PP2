c6a70f8d8186189dbf8f26ccda663961eea86f67
Daniyarr <43133652+Daniyarr@users.noreply.github.com>
2019-02-15 15:40:20 +0600

    Create Program.cs

---
+++ Week2/Task 1/Task 1_1/Task 1_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();          //создаю новую строку
+            for (int i = 0; i < s.Length; i++)       //пробегаюсь по массиву
+            {
+                if (s[i] != s[s.Length - 1 - i])     //проверяю строку на полиндромность
+                {
+                    Console.WriteLine("No");
+                    break;
+                }
+
+            }
+            Console.WriteLine("Yes");               //если проходит проверку то вывожу YES
+        }
+    }
+}
