3ccb254a2fdb6297f00b17c616f01d56aa593af9
Daniyarr <43133652+Daniyarr@users.noreply.github.com>
2019-02-15 15:24:33 +0600

    Create Program.cs

---
+++ Week1/Task3/Task3/Task3/Task3_1/Task3_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] a = (Console.ReadLine()).Split(); // переводим в int первую строку
+            string[] b = new string[n * 2]; // создаем новый массив в которым будут храниться числа с дубликатами
+            for (int i = 0; i < n; i++) // пробегаемся по циклу
+            {
+                b[i * 2] = a[i]; // и делаем дубликаты
+                b[i * 2 + 1] = a[i];
+            }
+
+            for (int i = 0; i < n * 2; i++) // выводим числа
+            {
+                Console.Write(b[i] + " ");
+            }
+        }
+    }
+}
